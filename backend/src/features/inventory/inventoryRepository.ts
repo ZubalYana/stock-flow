af48e0019221fd933a0f4adca151d79c800dca19
Yana <zubalana0@gmail.com>
2026-08-14 05:23:21 +0300

    fix: stick to consistent pattern in the repositories

--- backend/src/features/inventory/inventoryRepository.ts
+++ backend/src/features/inventory/inventoryRepository.ts
@@ -1,25 +1,23 @@
 import type { InventoryDTO } from "./inventorySchema";
-import { prisma } from "../../config/prisma";
 import type { Prisma } from "../../generated/prisma/client" 
-
 
 export const inventoryRepository = {
     async create(client: Prisma.TransactionClient, itemId: string, data: InventoryDTO){
         return await client.inventory.create({data: {itemId, warehouseId: data.warehouseId, quantity: data.quantity}})
     },
-    async update(itemId: string, warehouseId: string, data: InventoryDTO){
-        return await prisma.inventory.update({where: {warehouseId_itemId: {warehouseId, itemId}}, data: {quantity: data.quantity}})
+    async update(client: Prisma.TransactionClient, itemId: string, warehouseId: string, data: InventoryDTO){
+        return await client.inventory.update({where: {warehouseId_itemId: {warehouseId, itemId}}, data: {quantity: data.quantity}})
     },
-    async delete(itemId: string, warehouseId: string){
-        return await prisma.inventory.delete({where: {warehouseId_itemId: {warehouseId, itemId}}})
+    async delete(client: Prisma.TransactionClient, itemId: string, warehouseId: string){
+        return await client.inventory.delete({where: {warehouseId_itemId: {warehouseId, itemId}}})
     },
-    async getAllByWarehouse(warehouseId: string){
-        return await prisma.inventory.findMany({where: {warehouseId}})
+    async getAllByWarehouse(client: Prisma.TransactionClient, warehouseId: string){
+        return await client.inventory.findMany({where: {warehouseId}})
     },
-    async getAllByItem(itemId: string){
-        return await prisma.inventory.findMany({where: {itemId}})
+    async getAllByItem(client: Prisma.TransactionClient, itemId: string){
+        return await client.inventory.findMany({where: {itemId}})
     },
-    async getAll(){
-        return await prisma.inventory.findMany()
+    async getAll(client: Prisma.TransactionClient){
+        return await client.inventory.findMany()
     }
 }
--- backend/src/features/items/itemRepository.ts
+++ backend/src/features/items/itemRepository.ts
@@ -1,20 +1,19 @@
-import { prisma } from "../../config/prisma"
 import type { ItemDTO } from "./itemSchema"
 import type { Prisma } from "../../generated/prisma/client" 
 export const itemRepository = {
     async create(client: Prisma.TransactionClient, data: ItemDTO){
         return await client.item.create({data: {name: data.name}})
     },
-    async update(id: string, data: ItemDTO){
-        return await prisma.item.update({where: {id}, data: {name: data.name}})
+    async update(client: Prisma.TransactionClient, id: string, data: ItemDTO){
+        return await client.item.update({where: {id}, data: {name: data.name}})
     },
-    async delete(id: string){
-        return await prisma.item.delete({where: {id}})
+    async delete(client: Prisma.TransactionClient, id: string){
+        return await client.item.delete({where: {id}})
     },
-    async getById(id: string){
-        return await prisma.item.findUnique({where: {id}})
+    async getById(client: Prisma.TransactionClient, id: string){
+        return await client.item.findUnique({where: {id}})
     },
-    async getAll(){
-        return await prisma.item.findMany()
+    async getAll(client: Prisma.TransactionClient){
+        return await client.item.findMany()
     },
 }

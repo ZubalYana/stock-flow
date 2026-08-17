9d653f7f6ca800dd7983997bb71daaa10af7ca37
Yana <zubalana0@gmail.com>
2026-08-17 16:23:41 +0300

    feat: set up fonts, fix the layout

--- frontend/src/components/auth/AuthLayout.tsx
+++ frontend/src/components/auth/AuthLayout.tsx
@@ -6,15 +6,12 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="w-full max-h-screen relative flex flex-col lg:flex-row">
+    <div className="w-full h-screen max-h-screen relative flex flex-col">
         <div className="absolute top-5 left-5 lg:top-8 lg:left-10 z-10">
       <Logo />
       </div>
-      <div className="hidden lg:flex lg:w-1/2 items-center justify-center bg-primary/10 p-16">
-        <img src="/trip-illustration.svg" alt="trip illustration" className="w-full max-w-[90%]" />
-      </div>
 
-      <div className="w-full lg:w-1/2 flex items-center justify-center p-5 py-24 lg:py-5">
+      <div className="w-full h-full flex items-center justify-center">
         {children}
       </div>
     </div>
